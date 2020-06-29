eff9b8eb7bd030b7b58d97895c178106fd8c1c44
Chris Seto <chriskseto@gmail.com>
2020-06-29 17:48:07 -0400

    Allow a prefix to be added zone and region (#32)

--- locality-checker/main.go
+++ locality-checker/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"os"
 
@@ -10,14 +11,20 @@ import (
 
 const defaultLocalityMountPath = "/etc/cockroach-locality"
 
+var prefix = flag.String("prefix", "", "prefix")
+
 func main() {
+	flag.Parse()
+
 	ctx := context.Background()
+
 	var localityMountPath string
-	if len(os.Args) == 2 {
-		localityMountPath = os.Args[1]
+	if len(flag.Args()) == 2 {
+		localityMountPath = flag.Args()[1]
 	} else {
 		localityMountPath = defaultLocalityMountPath
 	}
+
 	nodeName := os.Getenv("KUBERNETES_NODE")
 	if nodeName == "" {
 		log.Fatal("KUBERNETES_NODE must be set")
@@ -32,6 +39,7 @@ func main() {
 		NodeName:             nodeName,
 		WritePath:            localityMountPath,
 		ErrorOnMissingLabels: errorOnMissingLabels == "1",
+		Prefix:               *prefix,
 	}
 	if err := l.WriteLocality(ctx); err != nil {
 		log.Fatalf("error writing locality: %v", err)
